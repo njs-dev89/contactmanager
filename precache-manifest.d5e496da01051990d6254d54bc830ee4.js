3ee54f6cbd7a348ad44ed7ce3c22aeb0d0f1579f
njs-dev89 <najamkhan89@gmail.com>
2020-04-14 12:49:05 +0500

    Updates

---
+++ precache-manifest.d5e496da01051990d6254d54bc830ee4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b506902770d09503b1cab03ea7a97f4e",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "7df21d4d5740a2395a7e",
+    "url": "/contactmanager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "826de693a08195926e9a",
+    "url": "/contactmanager/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "7df21d4d5740a2395a7e",
+    "url": "/contactmanager/static/js/2.fbbcf7cb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.fbbcf7cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "826de693a08195926e9a",
+    "url": "/contactmanager/static/js/main.81535ec2.chunk.js"
+  },
+  {
+    "revision": "98d49c782551fbed3aca",
+    "url": "/contactmanager/static/js/runtime-main.53501af3.js"
+  }
+]);
